e8186298ca5852ba19ac0ddd028cb37d475a54fc
Thai Luong <ngocthai199x@gmail.com>
2025-04-05 23:36:24 +0700

    Completed shopping cart page and functions

---
+++ client18/src/app/features/cart/cart-item/cart-item.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, input, Input } from '@angular/core';
+import { CartItem } from '../../../shared/models/cart';
+import { RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../../core/services/cart.service';
+
+@Component({
+  selector: 'app-cart-item',
+  standalone: true,
+  imports: [RouterLink, MatIcon, MatButton, CurrencyPipe],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss',
+})
+export class CartItemComponent {
+  item = input.required<CartItem>();
+  cartService = inject(CartService);
+
+  encrementQuantity() {
+    this.cartService.addItemToCart(this.item());
+  }
+  decrementQuantity() {
+    this.cartService.removeItemFromCart(this.item().productId);
+  }
+  removeItemFromCart() {
+    this.cartService.removeItemFromCart(
+      this.item().productId,
+      this.item().quantity
+    );
+  }
+}
--- client18/src/app/features/shop/product-details/product-details.component.ts
+++ client18/src/app/features/shop/product-details/product-details.component.ts
@@ -8,6 +8,8 @@ import { MatIcon } from '@angular/material/icon';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { CartService } from '../../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -20,14 +22,19 @@ import { MatDivider } from '@angular/material/divider';
     MatInput,
     MatLabel,
     MatDivider,
+    FormsModule,
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent implements OnInit {
   private shopService = inject(ShopService);
+  private cartService = inject(CartService);
   private activatedRoute = inject(ActivatedRoute);
   product?: Product;
+  quantityInCart = 0;
+  quantity = 1;
+
   ngOnInit(): void {
     this.loadProduct();
   }
@@ -36,9 +43,32 @@ export class ProductDetailsComponent implements OnInit {
     console.log(id);
     if (!id) return;
     this.shopService.getProduct(+id).subscribe({
-      next: (product) => (this.product = product),
+      next: (product) => {
+        this.product = product;
+        this.updateQuantityInCart();
+      },
       error: (err) => console.log(err),
     });
     console.log(this.product);
+  }
+  updateCart() {
+    if (!this.product) return;
+    if (this.quantity > this.quantityInCart) {
+      const itemsToAdd = this.quantity - this.quantityInCart;
+      this.quantityInCart += itemsToAdd;
+      this.cartService.addItemToCart(this.product, itemsToAdd);
+    } else {
+      const itemsToRemove = this.quantityInCart - this.quantity;
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove);
+    }
+  }
+  updateQuantityInCart() {
+    const cart = this.cartService.cart();
+    this.quantityInCart =
+      cart?.items.find((x) => x.productId == this.product?.id)?.quantity || 0;
+    this.quantity = this.quantityInCart || 1;
+  }
+  getButtonText() {
+    return this.quantityInCart > 0 ? 'Update cart' : 'Add to cart';
   }
 }
